cc12cb4fddc1a4fbcdf5791c81add1c9b1154849
Denys Voskoboynyk <denys.voskoboynyk@fortum.com>
2021-10-14 14:47:47 +0200

    Prepare base phone number sign in POC

--- App.tsx
+++ App.tsx
@@ -3,32 +3,60 @@ import React, { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
-  const [confirm, setConfirm] = useState(null);
+  const testPhoneNumber = "+16505551234";
 
+  const [confirm, setConfirm] = useState(null);
   const [code, setCode] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState(testPhoneNumber);
+  const [signedIn, setSignedIn] = useState(false);
+
+  const cleanup = () => {
+    setConfirm(null);
+    setCode("");
+    setSignedIn(false);
+    setPhoneNumber(testPhoneNumber);
+  };
 
   const signInWithPhone = async (phoneNumber: string) => {
-    console.log("clickie click");
-    const confirmation = await auth().signInWithPhoneNumber(phoneNumber);
-    setConfirm(confirmation);
+    try {
+      const confirmation = await auth().signInWithPhoneNumber(phoneNumber);
+      setConfirm(confirmation);
+    } catch (error) {
+      console.log("Error during phone signing: ", error);
+    }
   };
 
   const confirmCode = async () => {
     try {
       await confirm.confirm(code);
+      setSignedIn(true);
     } catch (error) {
-      console.log("Invalid code.");
+      console.log("Code confirmation error: ", error);
     }
   };
 
-  if (!confirm) {
+  if (signedIn) {
     return (
       <View style={styles.container}>
-        <Text style={styles.item}>Test firebase phone sign in</Text>
+        <Text style={styles.item}>{`Signed in with phone ${phoneNumber}`}</Text>
+        <Button color={"#CFBD3A"} title={"Again"} onPress={cleanup} />
+      </View>
+    );
+  }
+
+  if (confirm) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.tip}>Test code: 123456</Text>
+        <TextInput
+          style={styles.input}
+          value={code}
+          onChangeText={(text: string) => setCode(text)}
+        />
         <Button
-          color={"#78A0D4"}
-          title={"Phone number sign in"}
-          onPress={() => signInWithPhone("")}
+          color={"#3E9458"}
+          title="Confirm Code"
+          onPress={() => confirmCode()}
         />
       </View>
     );
@@ -36,11 +64,17 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <TextInput value={code} onChangeText={(text: string) => setCode(text)} />
+      <Text style={styles.tip}>{`Test number: ${testPhoneNumber}`}</Text>
+      <Text style={styles.item}>Test firebase phone sign in</Text>
+      <TextInput
+        style={styles.input}
+        value={phoneNumber}
+        onChangeText={(text: string) => setPhoneNumber(text)}
+      />
       <Button
-        color={"#3E9458"}
-        title="Confirm Code"
-        onPress={() => confirmCode()}
+        color={"#78A0D4"}
+        title={"Phone number sign in"}
+        onPress={() => signInWithPhone(phoneNumber)}
       />
     </View>
   );
@@ -53,8 +87,22 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  input: {
+    backgroundColor: "#F7F7F7",
+    height: 36,
+    width: "36%",
+    marginBottom: 16,
+    borderWidth: 1,
+    borderColor: "#555555",
+    padding: 10,
+  },
   item: {
     marginBottom: 8,
     color: "#222222",
+  },
+  tip: {
+    marginBottom: 4,
+    fontSize: 8,
+    color: "#999999",
   },
 });
